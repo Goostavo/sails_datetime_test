6fff45b0cd9543016c5a1936921e1f17e6377a82
Gustavo Garcia <goostavobg@gmail.com>
2019-01-21 18:39:56 -0200

    Created sails 1.0 project with empty tests

---
+++ sails1_0/test/bootstrap.test.js
@@ -0,0 +1,30 @@
+/**
+* Testing boostraping
+*
+* @description :: Lifts the sails server for testing
+*/
+var Sails = require('sails');
+
+// Global before hook
+before(function (done) {
+  this.timeout(120000);//Long timeout. Sails lifting on first time can be slow
+  // Lift Sails with test database
+  Sails.lift({
+    log: {
+      level: 'error'
+    }
+  }, function(err) {
+    if (err)
+      return done(err);
+    else
+      return done();
+  });
+
+});
+
+// Global after hook
+after(function (done) {
+  // console.log('Lowering Sails');
+  Sails.lower(done);
+});
+
